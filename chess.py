7544a94941a034f14c43f40fbd4d799acf9837ca
M0r13n <leonrichter@fastmail.com>
2019-02-19 16:10:57 +0100

    gen_moves for pseudo legal moves is 90% complete

--- chess.py
+++ chess.py
@@ -1,3 +1,13 @@
+"""
+A chess game
+
+todo:
+- Implement Game Loop
+- Implement an uci parser
+- Fix todos in logic code
+- Up to now only pseudo legal moves are provided -> check for legality
+"""
+
 UNIVERSE = 0xffffffffffffffff
 EMPTY = 0
 
@@ -48,6 +58,8 @@ BLACK = 0
 
 SQUARE_NAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
+BASEBOARD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 
 def _lsb(b: B_BOARD):
     return (b & -b).bit_length() - 1
@@ -70,6 +82,15 @@ def _scan_lsb_first(b: B_BOARD):
         r = b & -b
         yield r.bit_length() - 1
         b ^= r
+
+
+def _scan_single_values(b):
+    v = 1
+    while b:
+        if b & 1:
+            yield v
+        b = b >> 1
+        v = v << 1
 
 
 def _set_bit(b: B_BOARD, index, x):
@@ -113,29 +134,76 @@ def _shift_left_left(b: B_BOARD):
 
 
 def _shift_right_up(b: B_BOARD):
-    return (b << 9) & ~A_LINE & UNIVERSE
+    return (b & ~A_LINE) << 9 & UNIVERSE
 
 
 def _shift_right_down(b: B_BOARD):
-    return (b >> 7) & ~A_LINE
+    return (b & ~A_LINE) >> 7
 
 
 def _shift_left_up(b: B_BOARD):
-    return (b << 7) & ~H_LINE & UNIVERSE
+    return (b & ~H_LINE) << 7 & UNIVERSE
 
 
 def _shift_left_down(b: B_BOARD):
-    return (b >> 9) & ~H_LINE
+    return (b & ~H_LINE) >> 9
 
 
-DIRECTIONS = {
-    'P': ((_shift_up, _shift_up_up, _shift_left_up, _shift_right_up)),
-    'N': None,
-    'B': None,
-    'R': None,
-    'Q': None,
-    'K': None
+DIAGONALS = [UL, DL, DR, UR] = range(4)
+
+DIAGONAL_MOVS = {
+    UL: lambda x: _shift_up(_shift_left(x)),
+    DL: lambda x: _shift_down(_shift_left(x)),
+    DR: lambda x: _shift_down(_shift_right(x)),
+    UR: lambda x: _shift_up(_shift_right(x))
+
 }
+
+KNIGHT_MOVS = [
+    lambda x: _shift_left(_shift_down_down(x)),
+    lambda x: _shift_left(_shift_up_up(x)),
+    lambda x: _shift_right(_shift_up_up(x)),
+    lambda x: _shift_right(_shift_down_down(x)),
+    lambda x: _shift_up(_shift_right_right(x)),
+    lambda x: _shift_up(_shift_left_left(x)),
+    lambda x: _shift_down(_shift_right_right(x)),
+    lambda x: _shift_down(_shift_left_left(x)),
+]
+
+KING_MOVS = [_shift_right, _shift_right_down, _shift_down, _shift_left_down,
+             _shift_left, _shift_left_up, _shift_up, _shift_right_up]
+
+LINE_MOVS = [_shift_up, _shift_right, _shift_down, _shift_left]
+
+
+def traverse_diagonal(square, diagonal, occupied, enemies):
+    moves = 0
+    sq = square
+    shift = DIAGONAL_MOVS[diagonal]
+    while True:
+        sq = shift(sq)
+        if sq & occupied:
+            break
+        moves |= sq
+        if sq & enemies or sq == 0:
+            break
+
+    return moves
+
+
+def traverse_line(square, shift, occupied, enemies):
+    moves = 0
+    sq = square
+
+    while True:
+        sq = shift(sq)
+        if sq & occupied:
+            break
+        moves |= sq
+        if sq & enemies or sq == 0:
+            break
+
+    return moves
 
 
 def b_board_to_str(b: B_BOARD) -> str:
@@ -172,25 +240,38 @@ class Board:
     This class stores all pieces for a game
     """
 
-    def __init__(self):
+    def __init__(self, fen=None):
         """
         new game
         """
-        # White
-        self.w_king = 576460752303423488
-        self.w_pawns = 71776119061217280
-        self.w_queens = 1152921504606846976
-        self.w_bishops = 2594073385365405696
-        self.w_knights = 4755801206503243776
-        self.w_rooks = 9295429630892703744
+        # White pieces
+        self.b_king = 0
+        self.b_pawns = 0
+        self.b_queens = 0
+        self.b_bishops = 0
+        self.b_knights = 0
+        self.b_rooks = 0
 
-        # Black
-        self.b_king = 8
-        self.b_pawns = 65280
-        self.b_queens = 16
-        self.b_bishops = 36
-        self.b_knights = 66
-        self.b_rooks = 129
+        # Black pieces
+        self.w_king = 0
+        self.w_pawns = 0
+        self.w_queens = 0
+        self.w_bishops = 0
+        self.w_knights = 0
+        self.w_rooks = 0
+
+        # state
+        self.active_player = 1
+        self.black_king_side_castle_right = True
+        self.black_queen_side_castle_right = True
+        self.white_king_side_castle_right = True
+        self.white_queen_side_castle_right = True
+        self.ep_move = None
+        self.half_move_clock = 0
+        self.move_number = 0
+
+        fen = BASEBOARD if fen is None else fen
+        self.from_fen(fen)
 
     def __repr__(self):
         return b_board_to_str(self.all_pieces)
@@ -198,7 +279,7 @@ class Board:
     def __str__(self):
         def populate(li, pieces, symbol):
             for k in _scan_lsb_first(pieces):
-                li[k] = SYMBOLS[symbol]
+                li[63 - k] = SYMBOLS[symbol]
 
         def name_bar() -> str:
             return '{offset}{row}{offset}\n'.format(offset=2 * ' ', row=' '.join(SQUARE_NAMES))
@@ -235,6 +316,85 @@ class Board:
         t += name_bar()
         return t
 
+    def reset(self):
+        # White pieces
+        self.b_king = 0
+        self.b_pawns = 0
+        self.b_queens = 0
+        self.b_bishops = 0
+        self.b_knights = 0
+        self.b_rooks = 0
+
+        # Black pieces
+        self.w_king = 0
+        self.w_pawns = 0
+        self.w_queens = 0
+        self.w_bishops = 0
+        self.w_knights = 0
+        self.w_rooks = 0
+
+        # state
+        self.active_player = 1
+        self.black_king_side_castle_right = True
+        self.black_queen_side_castle_right = True
+        self.white_king_side_castle_right = True
+        self.white_queen_side_castle_right = True
+        self.ep_move = None
+        self.half_move_clock = 0
+        self.move_number = 0
+
+    def from_fen(self, text):
+        # todo make more robust
+
+        # reset all values
+        self.reset()
+
+        sq = 1 << 63
+
+        text = text.split(' ')
+        pieces = text[0]
+        for symbol in pieces:
+            if symbol.isdigit():
+                sq = sq >> int(symbol)
+            elif symbol in SYMBOLS.keys():
+                if symbol == 'p':
+                    self.b_pawns |= sq
+                elif symbol == 'P':
+                    self.w_pawns |= sq
+                elif symbol == 'r':
+                    self.b_rooks |= sq
+                elif symbol == 'R':
+                    self.w_rooks |= sq
+                elif symbol == 'n':
+                    self.b_knights |= sq
+                elif symbol == 'N':
+                    self.w_knights |= sq
+                elif symbol == 'b':
+                    self.b_bishops |= sq
+                elif symbol == 'B':
+                    self.w_bishops |= sq
+                elif symbol == 'q':
+                    self.b_queens |= sq
+                elif symbol == 'Q':
+                    self.w_queens |= sq
+                elif symbol == 'k':
+                    self.b_king |= sq
+                elif symbol == 'K':
+                    self.w_king |= sq
+                sq = sq >> 1
+        self.active_player = True if text[1] == 'w' else False
+        self.black_king_side_castle_right = 'k' in text[2]
+        self.black_queen_side_castle_right = 'q' in text[2]
+        self.white_king_side_castle_right = 'K' in text[2]
+        self.white_queen_side_castle_right = 'q' in text[2]
+        self.ep_move = text[3] if text[3] != "-" else None
+        self.half_move_clock = int(text[4])
+        self.move_number = int(text[5])
+
+    def to_fen(self):
+        # todo implement
+        pass
+
     @property
     def all_white_pieces(self):
         return self.w_king | self.w_pawns | self.w_bishops | self.w_knights | self.w_queens | self.w_rooks
@@ -265,8 +425,62 @@ class Board:
             p = self.w_rooks if player else self.b_rooks
         return p
 
-    def gen_moves_white(self):
-        pass
+    def gen_moves(self, player, exclude_attacked_fields=False):
+        enemies = self.all_black_pieces if player else self.all_white_pieces
+        occupied = self.all_white_pieces if player else self.all_black_pieces
+        moves = 0
 
-    def gen_moves_black(self):
-        pass
+        # PAWNS
+        pawns = self.w_pawns if player else self.b_pawns
+
+        # one step
+        mov_v = _shift_up if player else _shift_down
+        pos = mov_v(pawns) & ~occupied & ~enemies
+        moves |= pos
+
+        # two steps
+        pos = pos & RANK_6 if player else pos & RANK_3
+        pos = mov_v(pos) & ~occupied & ~enemies
+        moves |= pos
+
+        # diagonal attacks
+        mov_ds = (_shift_left_up, _shift_right_up) if player else (_shift_left_down, _shift_right_down)
+        for mov_d in mov_ds:
+            pos = mov_d(pawns) & ~occupied & enemies
+            moves |= pos
+
+        # todo en passant
+
+        # KING
+        king = self.w_king if player else self.b_king
+        attacked_fields = 0 if exclude_attacked_fields else self.gen_moves(not player, True)
+
+        for mov in KING_MOVS:
+            moves |= mov(king) & ~occupied & ~attacked_fields
+
+        # todo castle
+
+        # KNIGHTS
+        knights = self.w_knights if player else self.b_knights
+        for mov in KNIGHT_MOVS:
+            moves |= mov(knights) & ~occupied
+
+        # ROOKS
+        rooks = self.w_rooks if player else self.b_rooks
+        for shift in LINE_MOVS:
+            moves |= traverse_line(rooks, shift, occupied, enemies)
+
+        # BISHOPS
+        bishops = self.w_bishops if player else self.b_bishops
+        for diagonal in DIAGONALS:
+            moves |= traverse_diagonal(bishops, diagonal, occupied, enemies)
+
+        # QUEENS
+        queens = self.w_queens if player else self.b_queens
+        for shift in LINE_MOVS:
+            moves |= traverse_line(queens, shift, occupied, enemies)
+
+        for diagonal in DIAGONALS:
+            moves |= traverse_diagonal(queens, diagonal, occupied, enemies)
+
+        return moves
--- tests.py
+++ tests.py
@@ -61,7 +61,80 @@ class SetBit(BaseTest):
         assert (_set_bit(7, 0, 0) == 6)
 
 
-TESTS = [LSBTest(), MSBTest(), ScanLSBFirstTest(), SetBit(), ]
+class TestFENNotation(BaseTest):
+    def __init__(self):
+        super(TestFENNotation, self).__init__(name="Test FEN Notation.")
+
+    def run(self):
+        from chess import Board, RANK_7, RANK_2
+
+        # new game is loaded when no fence is passed
+        b = Board()
+        assert (b.active_player == 1)
+        assert b.black_king_side_castle_right
+        assert b.black_queen_side_castle_right
+        assert b.white_king_side_castle_right
+        assert b.white_queen_side_castle_right
+        assert b.move_number == 1
+        assert b.half_move_clock == 0
+        assert b.all_black_pieces == int('1111111111111111', 2) << 48
+        assert b.all_white_pieces == int('1111111111111111', 2)
+        assert b.b_pawns == RANK_2
+        assert b.w_pawns == RANK_7
+        assert b.b_king == int('00001000', 2) << 56
+        assert b.w_king == int('00001000', 2)
+        assert b.b_queens == int('00010000', 2) << 56
+        assert b.w_queens == int('00010000', 2)
+        assert b.b_knights == int('01000010', 2) << 56
+        assert b.w_knights == int('01000010', 2)
+        assert b.b_bishops == int('00100100', 2) << 56
+        assert b.w_bishops == int('00100100', 2)
+        assert b.b_rooks == int('10000001', 2) << 56
+        assert b.w_rooks == int('10000001', 2)
+
+        # whites moves pawn two steps
+        b.from_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+        assert b.ep_move == "e3"
+        assert b.active_player == 0
+        assert b.w_pawns == int('00001000', 2) << 24 | int('11110111', 2) << 8
+
+        # black also moves a pawn two steps
+        b.from_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2")
+        assert b.ep_move == "c6"
+        assert b.move_number == 2
+        assert b.active_player == 1
+        assert b.w_pawns == int('00001000', 2) << 24 | int('11110111', 2) << 8
+        assert b.b_pawns == int('00100000', 2) << 32 | int('11011111', 2) << 48
+
+        # whites moves its knight
+        b.from_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2")
+        assert b.ep_move is None
+        assert b.active_player == 0
+        assert b.black_king_side_castle_right
+        assert b.black_queen_side_castle_right
+        assert b.white_king_side_castle_right
+        assert b.white_queen_side_castle_right
+        assert b.w_pawns == int('00001000', 2) << 24 | int('11110111', 2) << 8
+        assert b.b_pawns == int('00100000', 2) << 32 | int('11011111', 2) << 48
+        assert b.w_knights == int('01000000', 2) | int('00000100', 2) << 16
+
+
+class TestMovements(BaseTest):
+    def __init__(self):
+        super(TestMovements, self).__init__(name="Test movements for pawns")
+
+    def run(self):
+        from chess import Board, RANK_5, D_LINE
+
+        b = Board()
+        assert b.gen_moves(0) == int(8 * '1', 2) << 40 | int(8 * '1', 2) << 32
+        assert b.gen_moves(1) == int(8 * '1', 2) << 16 | int(8 * '1', 2) << 24
+
+        b.from_fen("8/8/8/8/3r4/8/8/8 b KQkq - 1 2")
+        assert b.gen_moves(0) == (RANK_5 | D_LINE) & ~b.all_black_pieces
+
+
+TESTS = [LSBTest(), MSBTest(), ScanLSBFirstTest(), SetBit(), TestMovements(), TestFENNotation(), ]
 
 
 def run_all_tests():
